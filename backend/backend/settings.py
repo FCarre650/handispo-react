e078bc0d7fc44bd9f5605873ba501c68efa5e594
Maud Peleran <m.peleran@gmail.com>
2025-02-26 09:17:15 +0100

    env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -141,3 +141,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
