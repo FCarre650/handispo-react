014fecd5c3c2bbe2446ca6b8260aa8c7fc8282ab
Maud Peleran <m.peleran@gmail.com>
2025-02-27 11:58:16 +0100

    data base updated with handicaps and sports

--- backend/app/admin.py
+++ backend/app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Association
+from .models import Association, Handicap, Sport
 
 admin.site.register(Association)
+admin.site.register(Handicap)
+admin.site.register(Sport)
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,27 +4,42 @@ import liste from '../assets/images/liste.png'
 import carte from '../assets/images/carte.png'
 import catalogue from '../assets/images/livre.png'
 import aide from '../assets/images/aide.png'
+import { Link } from 'react-router-dom';
 
 
 export default function Footer() {
     return (
         <div className='footer'>
             <footer>
+                <Link to="/">
                 <a href="accueil">
                     <img src={accueil} alt="accueil" className="accueil" />
                 </a>
+                </Link>
+
+                <Link to="/associations">
                 <a href="liste">
                     <img src={liste} alt="liste" className= "liste"/>
                 </a>
+                </Link>
+
+                <Link to="/map">
                 <a href="carte">
                     <img src={carte} alt="carte" className="carte" />
                 </a>
+                </Link>
+
+                <Link to="/catalog">
                 <a href="catalogue">
                     <img src={catalogue} alt="catalogue" className= "catalogue"/>
                 </a>
+                </Link>
+
+                <Link to="/help">
                 <a href="aide">
                     <img src={aide} alt="aide" className= "aide"/>
                 </a>
+                </Link>
             </footer>
         </div>
     );
